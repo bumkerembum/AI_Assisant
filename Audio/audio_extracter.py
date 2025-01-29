8e4256c3e1e7674f57668dd11c7203e15ee71a47
bumkerembum <krmdulger@gmail.com>
2025-01-29 20:56:05 +0300

    Added wav converter, audio splitter will be added

--- Audio/audio_extracter.py
+++ Audio/audio_extracter.py
@@ -1,2 +1,8 @@
-#deneme yaptık
+from pydub import AudioSegment
+
+
+def mp3_to_wav(path): # Convert mp3 to wav
+    sound = AudioSegment.from_mp3(path)
+    wav_sound = sound.export(path.replace('.mp3', '.wav'), format='wav')
+    return wav_sound
 
